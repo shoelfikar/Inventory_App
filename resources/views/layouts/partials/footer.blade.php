4cd3cdc8d4959c5722d55e1b6399a561f91440d3
shoelfikar <sulfikardi25@gmail.com>
2021-01-23 20:39:10 +0700

    add admin-lte template

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer text-center">
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+</footer>
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    @include('layouts.partials.styles')
+    <title>Inventory | @yield('title')</title>
+  </head>
+  <body class="hold-transition sidebar-mini layout-fixed">
+
+    <div class="wrapper">
+
+        @include('layouts.partials.navbar')
+        @include('layouts.partials.sidebar')
+
+
+        <div class="content-wrapper">
+          <div class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0">Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                  <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard v1</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Main content -->
+          <section class="content">
+            @yield('content')
+          </section>
+        </div>
+
+        @include('layouts.partials.footer')
+      </div>
+
+
+      @include('layouts.partials.scripts')
+
+  </body>
+</html>
